31fc28b12c57dd76f250b1a7c8151a0de7bb5fcc
Monroe Getz <mgetz10@gmail.com>
2019-10-10 18:45:20 -0400

    touch events added

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ window.addEventListener('keydown', freePlay);
 window.addEventListener('keyup', handleKeyUp);
 keys.forEach(key => key.addEventListener('mousedown', freePlay));
 keys.forEach(key => key.addEventListener('touchstart', freePlay));
+keys.forEach(key => key.addEventListener('touchmove', freePlay));
+keys.forEach(key => key.addEventListener('touchend', handleKeyUp));
+keys.forEach(key => key.addEventListener('touchcancel', handleKeyUp));
 keys.forEach(key => key.addEventListener('mouseup', handleKeyUp));
 keys.forEach(key => key.addEventListener('mouseout', handleKeyUp));
